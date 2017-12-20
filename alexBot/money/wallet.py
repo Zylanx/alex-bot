523f607e5fa1a97fea8af1f407558a1e319284f6
Zylanx <zylanx@users.noreply.github.com>
2017-12-20 22:56:39 +1100

    Create the Currency and Wallet Classes add the start to a class based implementation of wallets

---
+++ alexBot/money/wallet.py
@@ -0,0 +1,15 @@
+__author__ = "Zylanx"
+
+""" New class-based implimentation of the user wallet 
+	A more robust impl using classes that impliment more verification checks
+	and other improvements
+"""
+
+class Currency():
+	""" A currency class that integrates verification as part of the class """
+	pass
+
+class Wallet():
+	""" A users wallet.
+		provides many functions for the managment and manipulation of wallets """
+	pass
