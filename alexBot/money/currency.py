586b7da617cec58f61991e846bd2ca3235e3314c
Zylanx <zylanx@users.noreply.github.com>
2017-12-21 16:51:21 +1100

    Update currency.py * Remove unneeded complexity from validation helpers * Add the validation helper: isTransferableAmount Used to check if it is something that _can_ be transfered * Add one error handler for the __init__ function If an infinity is used, trying to quantize it will raise an error. If it is an inf, it should just assign it without trying to quantize If that isn't the case, then it is a real error

--- alexBot/money/currency.py
+++ alexBot/money/currency.py
@@ -8,6 +8,8 @@ __author__ = "Zylanx"
 
 # TODO: Make this all compatible with asyncio
 # TODO: Reformat code to comply with authors code style
+# TODO: Impliment more checks related to the flags set by Decimal
+# TODO: Add error handling to make things a bit more responsive and not crash-happy
 
 from decimal import Decimal, ROUND_DOWN
 
@@ -19,6 +21,7 @@ class CurrencyValidationError(CurrencyError):
 	""" Error asserting that the validation check failed """
 	pass
 
+# TODO: Maybe make this just extend Decimal?
 class Currency():
 	""" A currency class that integrates verification as part of the class """
 
@@ -29,7 +32,16 @@ class Currency():
 	def __init__(self, value: str, places: int = 2):
 		self.places = places
 		self.placesDec = Decimal('10') ** -places # Gets the exponent for .quantize. 10**-2 = 0.01. TODO: give a better name
-		self.value = Decimal(value).quantize(self.placesDec, rounding = ROUND_DOWN) # Round and fit the value to a given place
+		# HACK: Might be better just to have a seperate one for infinite vals
+		try:
+			self.value = Decimal(value).quantize(self.placesDec, rounding = ROUND_DOWN) # Round and fit the value to a given place
+		except:
+			# Well shoot. That's no good!
+			# If it is inf, just assign without quantizing
+			if Decimal(value).is_infinite():
+				self.value = Decimal(value)
+			elif Decimal(value).is_nan():
+				raise CurrencyError() # TODO: Write message, use better error
 
 		if self.isNan():
 			raise CurrencyError() # TODO: Write an exception message
@@ -47,32 +59,39 @@ class Currency():
 		self.placesDec = Decimal('10') ** -places
 
 	# TODO: Think about changing verification funcs to instead raise errors
+	def isPositive(self) -> bool:
+		""" True when non-NaN, not signed, and not zero"""
+		return not self.value.is_zero() and not self.value.is_nan() and not self.value.is_signed()
+
 	def isPositiveOrZero(self) -> bool:
-		""" True when zero (+ or -) or a finite number that is not signed """
-		if (self.value.is_zero()) or (self.value.is_finite() and not self.value.is_signed()):
-			return True
-		else:
-			return False
+		""" True when zero (+ or -) or a non-NaN number that is not signed """
+		return self.value.is_zero() or (not self.value.is_nan() and not self.value.is_signed())
 
 	def isNegative(self) -> bool:
-		""" True when finite and signed and not zero """
-		if (not self.value.is_zero()) and (self.value.is_finite() and self.value.is_signed()):
-			return True
-		else:
-			return False
+		""" True when non-NaN, and signed, and not zero """
+		return (not self.value.is_zero()) and (not self.value.is_nan() and self.value.is_signed())
 
-	def isNumber(self) -> bool:
-		""" True when finite and not Nan """
-		if self.value.is_finite():
-			return True
-		else:
-			return False
+	# Positive has one, so why not Negative too
+	def isNegativeOrZero(self) -> bool:
+		""" True when zero or non-NaN and signed"""
+		return self.value.is_zero() or (not self.value.is_nan() and self.value.is_signed())
 
 	def isNan(self) -> bool:
-		if self.value.is_nan():
-			return True
-		else:
-			return False
+		""" True when NaN """
+		return self.value.is_nan()
+
+	def isPosInf(self) -> bool:
+		""" True when Inf """
+		return self.value.is_infinite() and not self.value.is_signed()
+
+	def isNegInf(self) -> bool:
+		""" True when -Inf """
+		return self.value.is_infinite() and self.value.is_signed()
+
+	def isTransferableAmount(self) -> bool:
+		""" Returns True if the value is a valid transferable amount
+			finite, not nan, not negative """
+		return self.isPositive() and not self.value.is_infinite()
 
 	def mult(self, multiplier: Decimal, rounding: str = ROUND_DOWN) -> Decimal:
 		""" Multiply the instance by a Decimal()
