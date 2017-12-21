aeabfcfb678c70d13d1b91468f6017ac7e6fa31e
Zylanx <zylanx@users.noreply.github.com>
2017-12-21 15:19:22 +1100

    Update wallet.py * Added a few more validation functions to Currency: isNan and isNumber * Added multiply and divide helper methods that just make the 	quantize step easier and can include integrated checks as needed * Changed the include to import only what is needed

--- alexBot/money/wallet.py
+++ alexBot/money/wallet.py
@@ -6,13 +6,20 @@ __author__ = "Zylanx"
 	and other improvements
 """
 
-import decimal
+# TODO: Btw Alex, if you are going to be using decimal, you should be keeping that consistent
+# If you use decimal, then it should be stored in the db as decimal/numeric
+# as that keeps it accurate and stops it losing precision.
+# The point of using decimal is that it can be used fixed point.
+# storing that as a float just wrecks that and makes it inaccurate,
+# especially since you _always_ want to be rounding down
+
+from decimal import Decimal, ROUND_DOWN
 
 class CurrencyError(Exception): # TODO: Change to a proper d.py error and handle it
 	""" A generic error raised by the currency class """
 	pass
 
-class CurrencyValidationError(Exception):
+class CurrencyValidationError(CurrencyError):
 	""" Error asserting that the validation check failed """
 	pass
 
@@ -25,13 +32,13 @@ class Currency():
 
 	def __init__(self, value: str, places: int = 2):
 		self.places = places
-		self.placesDec = decimal.Decimal('10') ** -places # Gets the exponent for .quantize. 10**-2 = 0.01. TODO: give a better name
-		self.value = decimal.Decimal(value).quantize(self.placesDec, rounding = decimal.ROUND_DOWN) # Round and fit the value to a given place
+		self.placesDec = Decimal('10') ** -places # Gets the exponent for .quantize. 10**-2 = 0.01. TODO: give a better name
+		self.value = Decimal(value).quantize(self.placesDec, rounding = ROUND_DOWN) # Round and fit the value to a given place
 
-		if self.value.is_nan():
+		if self.isNan():
 			raise CurrencyError() # TODO: Write an exception message
 
-	def isPositive(self) -> bool:
+	def isPositiveOrZero(self) -> bool:
 		""" True when zero (+ or -) or a finite number that is not signed """
 		if (self.value.is_zero) or (self.value.is_finite() and not self.value.is_signed()):
 			return True
@@ -45,7 +52,38 @@ class Currency():
 		else:
 			return False
 
-	def quantize(self, rounding = decimal.ROUND_DOWN):
+	def isNumber(self) -> bool:
+		""" True when finite and not Nan """
+		if self.value.is_finite():
+			return True
+		else:
+			return False
+
+	def isNan(self) -> bool:
+		if self.value.is_nan():
+			return True
+		else:
+			return False
+
+	def mult(self, multiplier: Decimal, rounding: str = ROUND_DOWN) -> Decimal:
+		""" Multiply the instance by a Decimal()
+			These are only provided for ease of use by getting rid of the quantize() step """
+		value = self.value * multiplier
+		value = value.quantize(self.placesDec, rounding = rounding)
+		# TODO: insert checks
+		self.value = value
+		return value
+
+	def divide(self, divisor: Decimal, rounding: str = ROUND_DOWN) -> Decimal:
+		""" Divide the instance by a Decimal()
+			These are only provided for ease of use by getting rid of the quantize() step """
+		value = self.value / divisor
+		value = value.quantize(self.placesDec, rounding = rounding)
+		# TODO: insert checks
+		self.value = value
+		return value
+
+	def quantize(self, rounding = ROUND_DOWN):
 		""" Quantize the value back to the required currency format """
 		self.value = self.value.quantize(self.placesDec, rounding = rounding)
 
